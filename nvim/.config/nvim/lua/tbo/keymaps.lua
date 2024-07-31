b04d24edf90b306990db6045d319e0cd97f11125
Travis Napier <napesdotnet@gmail.com>
2024-07-31 17:28:45 -0400

    center screen on page up and down

--- nvim/.config/nvim/lua/tbo/keymaps.lua
+++ nvim/.config/nvim/lua/tbo/keymaps.lua
@@ -26,7 +26,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ Basic Autocommands ]]
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+--
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
